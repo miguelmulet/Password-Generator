56a19a1481d4e8d60f3aa7cf09653033eee1c606
Miguel Mulet <mmiguel032202@gmail.com>
2022-01-20 01:46:27 -0500

    added intial JS logic

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+// User Input
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmUppercase;
 
-
-
-
+// Addition for special, #s & Alphabet characters
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Uppercase Conversion
+space = [];
+var toUpper = function(x) {
+  return x.toUpperCase();
+};
+// Creates Var for Uppercase Conversion
+alpha2 = alpha.map(toUpper);
+var choices;
 
 var generateBtn = document.querySelector("#generate");
 
